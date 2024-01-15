0ace2a137c0c8b2b14b7b736bd9d567b9e8d0567
Nishu Goel <n.goel@epilot.cloud>
2024-01-15 12:48:39 +0100

    Updated overlay.yaml after fixing source journey.yaml

---
+++ examples/a-e2e-journey/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    epilot-journey = {
+      source  = "epilot-dev/epilot-journey"
+      version = "0.2.0"
+    }
+  }
+}
+
+provider "epilot-journey" {
+  # Configuration options
+}
