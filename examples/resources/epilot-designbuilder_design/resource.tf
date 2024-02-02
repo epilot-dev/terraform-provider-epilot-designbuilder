4ccef7018611ef10b2e9fbffd17ef01cd9512ca1
Nishu Goel <n.goel@epilot.cloud>
2024-02-02 15:49:23 +0100

    Fix the design provider

--- examples/resources/epilot-designbuilder_design/resource.tf
+++ examples/resources/epilot-designbuilder_design/resource.tf
@@ -1,41 +1,69 @@
+terraform {
+  required_providers {
+    epilot-designbuilder = {
+      source  = "epilot-dev/epilot-designbuilder"
+      version = "0.6.5"
+    }
+  }
+}
+
+variable "custom_authorizer" {
+  type        = string
+}
+
+provider "epilot-designbuilder" {
+  # Configuration options
+  custom_authorizer = var.custom_authorizer
+}
+
 resource "epilot-designbuilder_design" "my_design" {
-  background = "...my_background..."
-  brand_id   = "...my_brand_id..."
-  brand_name = "...my_brand_name..."
-  created_at = "2021-01-30T08:30:00Z"
-  created_by = "...my_created_by..."
-  customer_portals = [
-    "{ \"see\": \"documentation\" }",
-  ]
-  custom_theme        = "...my_custom_theme..."
-  display_name        = "...my_display_name..."
-  edited              = true
-  emailaddress        = "...my_emailaddress..."
-  error               = "...my_error..."
-  file_type           = "LOGO"
-  font_family         = "...my_font_family..."
-  font_id             = "...my_font_id..."
-  font_name           = "...my_font_name..."
-  font_weight_bold    = "...my_font_weight_bold..."
-  font_weight_medium  = "...my_font_weight_medium..."
-  font_weight_regular = "...my_font_weight_regular..."
-  fullname            = "...my_fullname..."
-  id                  = "9415d9b1-61f7-40b7-8bf5-bc35ca152c3d"
-  last_modified_at    = "2022-09-22T19:52:48.561Z"
-  name                = "Raul Pacocha"
-  navbar              = "...my_navbar..."
-  paper               = "...my_paper..."
-  primary             = "...my_primary..."
-  s3_object_key       = "...my_s3_object_key..."
-  secondary           = "...my_secondary..."
-  style_name          = "...my_style_name..."
-  url                 = "...my_url..."
-  urls = [
-    "{ \"see\": \"documentation\" }",
-  ]
+  brand_id         = "...my_brand_id..."
+  brand_name       = "...my_brand_name..."
+  custom_theme     = "...my_custom_theme..."
+  edited           = true
+  last_modified_at = "...my_last_modified_at..."
+  style = {
+    consumer = {
+      customer_portals = [
+        "{ \"see\": \"documentation\" }",
+      ]
+      widgets = [
+        "{ \"see\": \"documentation\" }",
+      ]
+    }
+    logo = {
+      main = {
+        display_name  = "...my_display_name..."
+        file_type     = "FONT"
+        name          = "Betsy Conn"
+        s3_object_key = "...my_s3_object_key..."
+        url           = "...my_url..."
+      }
+    }
+    palette = {
+      background = "...my_background..."
+      error      = "...my_error..."
+      navbar     = "...my_navbar..."
+      paper      = "...my_paper..."
+      primary    = "...my_primary..."
+      secondary  = "...my_secondary..."
+    }
+    typography = {
+      font = {
+        font_family         = "...my_font_family..."
+        font_id             = "...my_font_id..."
+        font_name           = "...my_font_name..."
+        font_weight_bold    = "...my_font_weight_bold..."
+        font_weight_medium  = "...my_font_weight_medium..."
+        font_weight_regular = "...my_font_weight_regular..."
+        urls = [
+          "{ \"see\": \"documentation\" }",
+        ]
+      }
+      primary   = "...my_primary..."
+      secondary = "...my_secondary..."
+    }
+  }
+  style_name       = "bla"
   use_custom_theme = false
-  userid           = "...my_userid..."
-  widgets = [
-    "{ \"see\": \"documentation\" }",
-  ]
 }
