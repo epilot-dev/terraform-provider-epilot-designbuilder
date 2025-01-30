50aadc866305733a80f5c3bbab1bc140c48432c2
Viljami <v.kuosmanen@epilot.cloud>
2025-01-30 14:18:00 +0100

    Switch to openapi spec from s3

--- Makefile
+++ Makefile
@@ -1,8 +1,20 @@
 .PHONY: all docs
-all: docs speakeasy
+all: speakeasy docs
 
-speakeasy:
-	speakeasy generate sdk --lang terraform -o . -s designbuilder.yaml
+original.yaml:
+	curl https://docs.api.epilot.io/design-builder-api.yaml > original.yaml
+
+original_modified.yaml: original.yaml overlay.yaml
+	speakeasy overlay apply -s original.yaml -o overlay.yaml > original_modified.yaml
+
+overlay.yaml:
+	speakeasy overlay compare -s original.yaml -s original_modified.yaml > overlay.yaml
+
+speakeasy: 
+	$(eval TMP := $(shell mktemp -d))
+	curl https://docs.api.epilot.io/design-builder-api.yaml > $(TMP)/openapi.yaml
+	speakeasy overlay apply -s $(TMP)/openapi.yaml -o overlay.yaml > $(TMP)/final.yaml
+	speakeasy generate sdk --lang terraform -o . -s $(TMP)/final.yaml
 
 docs:
 	go generate ./...
--- examples/resources/epilot-designbuilder_design/resource.tf
+++ examples/resources/epilot-designbuilder_design/resource.tf
@@ -1,13 +1,12 @@
 resource "epilot-designbuilder_design" "my_design" {
   brand_id         = "{ \"see\": \"documentation\" }"
   brand_name       = "...my_brand_name..."
-  created_at       = "2021-01-30T08:30:00Z"
-  created_by       = "...my_created_by..."
-  custom_theme     = "{ \"see\": \"documentation\" }"
+  cashback         = "...my_cashback..."
+  coupon           = "...my_coupon..."
+  custom_css       = "...my_custom_css..."
+  custom_theme     = "...my_custom_theme..."
   design_id        = "4a062990-a6a3-11eb-9828-4f3da7d4935a"
-  edited           = true
-  id               = "4d59415d-9b16-41f7-8b78-bf5bc35ca152"
-  last_modified_at = "...my_last_modified_at..."
+  is_default       = true
   style            = "{ \"see\": \"documentation\" }"
   style_name       = "...my_style_name..."
   use_custom_theme = true
